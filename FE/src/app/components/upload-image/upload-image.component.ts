5afa51008b74223f5a9ef0f90ca736d617b8f4f8
dan933 <69749413+dan933@users.noreply.github.com>
2022-08-18 22:12:23 +1000

    added mock data using firBase api todo add helper functions for firestore api calls.

--- FE/src/app/components/upload-image/upload-image.component.ts
+++ FE/src/app/components/upload-image/upload-image.component.ts
@@ -3,6 +3,7 @@ import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { map, Observable } from 'rxjs';
 import { item } from 'src/app/models/no-sql-models';
+import { serverTimestamp, where } from 'firebase/firestore';
 
 @Component({
   selector: 'app-upload-image',
@@ -24,19 +25,22 @@ export class UploadImageComponent implements OnInit {
   image!: any;
   meta!: Observable<any>;
 
-  items!:Observable<any[]>;
+  //items!:Observable<any[]>;
 
   //private itemsCollection: AngularFirestoreCollection<item[]>;
 
-  //private itemsCollection: AngularFirestoreCollection<any>;
-  
+  private itemsCollection: AngularFirestoreCollection<any>;
+
 
   constructor(
     private storage: AngularFireStorage,
-    public firestore: AngularFirestore
+    public firestore: AngularFirestore,
   ) {
     const ref = this.storage.ref(`${this.organisationID}/orgLogo`);
     this.meta = ref.getDownloadURL();
+
+    // let test = this.firestore.collection('Organisations', ref => ref.where('"description"', '==', 'string'))
+    // test.valueChanges().pipe()
 
   //-----------------------example of getting all items ----------------------//
     // this.itemsCollection = firestore.collection<item[]>('Organisations')
@@ -53,29 +57,108 @@ export class UploadImageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // let object =
+    //   [
+    //   {
 
-//   let dateStamp = new Date();
+    //         name: "Tree's R US",
+    //         summary: "Tree's R US is an organisation specializing in growing, maintaining and selling trees.",
+    //         description:null,
+    //         activeStatus: true,
+    //         ABN:  "59683269382",
+    //         phone: "0455683457",
+    //         website: "https://www.treesrus.com.au",
+    //       img: "https://www.treesrus/logo-img.png",
+    //       createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
 
-//   let object = {
-//     name:null,
-//     description:"string",
-//     summary:"summary",
-//     ABN:1111111111,
-//     phone:"0411327261",
-//     website:"http://www.google.com.au",
-//     img:"thisisaurl",
-//     activeStatus:true,
-//     createdAt: dateStamp,
-//     updatedAt:null
-// }
-//   //example of adding an item
-//   this.itemsCollection = this.firestore.collection('/Organisations')
-//   let test = this.itemsCollection.add(object).catch(err => console.log(`Error:${err.message}`))
+    //         name: "Barry's Bakehouse",
+    //         summary: "Barry's bakehouse provides baked goods and fresh bread daily for local customers. Barry also provides free baked goods to the local nursing home",
+    //         description:null,
+    //         activeStatus: true,
+    //         ABN:  "27384372612",
+    //         phone: "0428473288",
+    //         website: "https://www.barriesbakery.com.au",
+    //         img: "https://www.barriesbakery/logo-img.png",
+    //         createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
+
+    //         name: "The Phone Zone",
+    //         summary: "The phone zone is a business provding phone repairs and sales.",
+    //         description:null,
+    //         activeStatus: false,
+    //         ABN:  "49382719557",
+    //         phone: "0455567982",
+    //         website: "https://www.phonezone.com",
+    //         img: "https://www.phonezone/logo-img.png", createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
+
+    //         name: "Social Moments",
+    //         summary: "Provides yummy social treats",
+    //         description:null,
+    //         activeStatus: true,
+    //         ABN:  "22999684773",
+    //         phone: "0433958444",
+    //         website: "https://www.socialmoments.com",
+    //         img: "https://www.socialmoments/logo-img.png", createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
+
+    //         name: "EdAble Flowers",
+    //         summary: "This organisation grows edible flowers for distribution",
+    //         description:null,
+    //         activeStatus: false,
+    //         ABN:  "48588334938",
+    //         phone: "0493959487",
+    //         website: "https://www.edablelfowers.com",
+    //         img: "https://www.edablelfowers/logo-img.png", createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
+
+    //         name: "Windy",
+    //         summary: "Windy is a non for profit organization that helps supply small wind turbines to unpriviled families ",
+    //         description:null,
+    //         activeStatus: true,
+    //         ABN:  "49557732843",
+    //         phone: "0438827348",
+    //         website: "https://www.Windy.com",
+    //         img: "https://www.Windy/logo-img.png", createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     },
+    //     {
+
+    //         name: "Doin Doughies",
+    //         summary: "Doin Doughies bakes fresh donuts for the town of Narrandera daily",
+    //         description:null,
+    //         activeStatus: false,
+    //         ABN:  "46392849511",
+    //         phone: "0489334598",
+    //         website: "https://www.doindoughies.com",
+    //         img: "https://www.doindoughies/logo-img.png", createdAt: serverTimestamp(),
+ 	 	//  	  	 updatedAt: null
+    //     }
+    // ]
+  //example of adding an item
+    // this.itemsCollection = this.firestore.collection('/Organisations')
+    // object.forEach(async (item) => {
+    //   await this.itemsCollection.add(item).catch(err => console.log(err))
+    // })
+  //this.itemsCollection.add(object[0]).catch(err => console.log(`Error:${err.message}`))
 
     this.meta.subscribe({
       next: (test) => this.image = test,
       error:(err) => console.log(err)
-     });
+    });
+
+
   }
 
   onFileSelected(event: any) {
