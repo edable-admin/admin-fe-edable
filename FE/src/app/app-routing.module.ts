8bfa5c43168a68be3f6d2467446b52000dbc7e21
Waderaide <103882777@student.swin.edu.au>
2022-08-18 15:26:11 +1000

    merged dev and navbar

--- FE/src/app/app-routing.module.ts
+++ FE/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { TempPageComponent } from './components/authentication/temp-page/temp-pa
 import { OrganisationComponent } from './components/organisation/organisation.component';
 import { UploadImageComponent } from './components/upload-image/upload-image.component';
 import { AuthGuard } from './shared/guard/auth.guard';
+import {TransactionsComponent} from './components/transactions/transactions.component'
 
   // User canActivate with [AuthGuard] to secure endpoints and ensure user is logged in
   // { path: 'dashboard', component: OrganisationComponent,  canActivate: [AuthGuard]},
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: OrganisationComponent},
+  { path: 'transaction', component: TransactionsComponent},
   { path: 'sign-in', component: SignInComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
 ];
