ec7b5f8e61e00dcbb5809e72749c2410c54adae4
dan933 <69749413+dan933@users.noreply.github.com>
2022-08-19 19:39:10 +1000

    Code cleaned up created a firestore service added firestore rules

--- FE/src/app/components/upload-image/upload-image.component.ts
+++ FE/src/app/components/upload-image/upload-image.component.ts
@@ -4,6 +4,7 @@ import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument 
 import { map, Observable } from 'rxjs';
 import { item } from 'src/app/models/no-sql-models';
 import { serverTimestamp, where } from 'firebase/firestore';
+import { FirestoreService } from 'src/app/services/firestore-services/firestore.service';
 
 @Component({
   selector: 'app-upload-image',
@@ -20,6 +21,8 @@ export class UploadImageComponent implements OnInit {
   //fire storage will create a file with the unique id and the file can be stored there in fire storage
   organisationID = "uid";
 
+  woo!: any;
+
   imageName!: any;
 
   image!: any;
@@ -35,129 +38,18 @@ export class UploadImageComponent implements OnInit {
   constructor(
     private storage: AngularFireStorage,
     public firestore: AngularFirestore,
+    public FirestoreService:FirestoreService
   ) {
+
     const ref = this.storage.ref(`${this.organisationID}/orgLogo`);
     this.meta = ref.getDownloadURL();
-
-    // let test = this.firestore.collection('Organisations', ref => ref.where('"description"', '==', 'string'))
-    // test.valueChanges().pipe()
-
-  //-----------------------example of getting all items ----------------------//
-    // this.itemsCollection = firestore.collection<item[]>('Organisations')
-    // this.items = this.itemsCollection.snapshotChanges()
-    // .pipe(
-    //   map( (actions:any) => actions.map( (a:any) => {
-    //     const data = a.payload.doc.data() as any;
-    //     const ID = a.payload.doc.id;
-    //     return { ID, ...data}
-    //   }))
-    // );
-    // this.items.subscribe(item => console.log(item))
-  //--------------------------------------------------------//
   }
 
   ngOnInit(): void {
-    // let object =
-    //   [
-    //   {
-
-    //         name: "Tree's R US",
-    //         summary: "Tree's R US is an organisation specializing in growing, maintaining and selling trees.",
-    //         description:null,
-    //         activeStatus: true,
-    //         ABN:  "59683269382",
-    //         phone: "0455683457",
-    //         website: "https://www.treesrus.com.au",
-    //       img: "https://www.treesrus/logo-img.png",
-    //       createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "Barry's Bakehouse",
-    //         summary: "Barry's bakehouse provides baked goods and fresh bread daily for local customers. Barry also provides free baked goods to the local nursing home",
-    //         description:null,
-    //         activeStatus: true,
-    //         ABN:  "27384372612",
-    //         phone: "0428473288",
-    //         website: "https://www.barriesbakery.com.au",
-    //         img: "https://www.barriesbakery/logo-img.png",
-    //         createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "The Phone Zone",
-    //         summary: "The phone zone is a business provding phone repairs and sales.",
-    //         description:null,
-    //         activeStatus: false,
-    //         ABN:  "49382719557",
-    //         phone: "0455567982",
-    //         website: "https://www.phonezone.com",
-    //         img: "https://www.phonezone/logo-img.png", createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "Social Moments",
-    //         summary: "Provides yummy social treats",
-    //         description:null,
-    //         activeStatus: true,
-    //         ABN:  "22999684773",
-    //         phone: "0433958444",
-    //         website: "https://www.socialmoments.com",
-    //         img: "https://www.socialmoments/logo-img.png", createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "EdAble Flowers",
-    //         summary: "This organisation grows edible flowers for distribution",
-    //         description:null,
-    //         activeStatus: false,
-    //         ABN:  "48588334938",
-    //         phone: "0493959487",
-    //         website: "https://www.edablelfowers.com",
-    //         img: "https://www.edablelfowers/logo-img.png", createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "Windy",
-    //         summary: "Windy is a non for profit organization that helps supply small wind turbines to unpriviled families ",
-    //         description:null,
-    //         activeStatus: true,
-    //         ABN:  "49557732843",
-    //         phone: "0438827348",
-    //         website: "https://www.Windy.com",
-    //         img: "https://www.Windy/logo-img.png", createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     },
-    //     {
-
-    //         name: "Doin Doughies",
-    //         summary: "Doin Doughies bakes fresh donuts for the town of Narrandera daily",
-    //         description:null,
-    //         activeStatus: false,
-    //         ABN:  "46392849511",
-    //         phone: "0489334598",
-    //         website: "https://www.doindoughies.com",
-    //         img: "https://www.doindoughies/logo-img.png", createdAt: serverTimestamp(),
- 	 	//  	  	 updatedAt: null
-    //     }
-    // ]
-  //example of adding an item
-    // this.itemsCollection = this.firestore.collection('/Organisations')
-    // object.forEach(async (item) => {
-    //   await this.itemsCollection.add(item).catch(err => console.log(err))
-    // })
-  //this.itemsCollection.add(object[0]).catch(err => console.log(`Error:${err.message}`))
-
     this.meta.subscribe({
-      next: (test) => this.image = test,
+      next: (resp) => this.image = resp,
       error:(err) => console.log(err)
     });
-
 
   }
 
@@ -173,7 +65,7 @@ export class UploadImageComponent implements OnInit {
         alert("invalid input")
         return;
       }
-    //todo the upload should be done on the create organisation level
+
       this.storage.upload(`${this.organisationID}/orgLogo`, file[0])
       this.uploadedImage.emit(file)
 
---
+++ FE/src/app/services/firestore-services/firestore.service.ts
@@ -0,0 +1,142 @@
+import { Injectable } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
+import { serverTimestamp } from 'firebase/firestore';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class FirestoreService {
+
+  constructor(
+    public firestore: AngularFirestore
+  ) { }
+
+  getOrganisations =  () => {
+    const organisationsCollection = this.firestore.collection('Organisations')
+      .valueChanges({idField:"ID"})
+
+    return organisationsCollection
+  }
+
+  getDonationItems = () => {
+    const itemsCollection = this.firestore.collection('Items')
+      .valueChanges({ idField: "ID" })
+
+    return itemsCollection
+  }
+
+  getOrganisationsDonationItems = () => {
+
+    //todo add parameters
+    const organisation = this.firestore.collection('Organisations').doc('3gbGtrF6KT1vhJ7mtlQS').ref;
+
+    const itemsCollection = this.firestore
+    .collection('Items', ref => ref.where('orgID', '==', organisation))
+    .valueChanges({idField:"ID"})
+
+    return itemsCollection
+  }
+
+  generateMockDate = () => {
+    let orgsToAdd =
+      [
+        {
+
+          name: "Tree's R US",
+          summary: "Tree's R US is an organisation specializing in growing, maintaining and selling trees.",
+          description: null,
+          activeStatus: true,
+          ABN: "59683269382",
+          phone: "0455683457",
+          website: "https://www.treesrus.com.au",
+          img: "https://www.treesrus/logo-img.png",
+          createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "Barry's Bakehouse",
+          summary: "Barry's bakehouse provides baked goods and fresh bread daily for local customers. Barry also provides free baked goods to the local nursing home",
+          description: null,
+          activeStatus: true,
+          ABN: "27384372612",
+          phone: "0428473288",
+          website: "https://www.barriesbakery.com.au",
+          img: "https://www.barriesbakery/logo-img.png",
+          createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "The Phone Zone",
+          summary: "The phone zone is a business provding phone repairs and sales.",
+          description: null,
+          activeStatus: false,
+          ABN: "49382719557",
+          phone: "0455567982",
+          website: "https://www.phonezone.com",
+          img: "https://www.phonezone/logo-img.png", createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "Social Moments",
+          summary: "Provides yummy social treats",
+          description: null,
+          activeStatus: true,
+          ABN: "22999684773",
+          phone: "0433958444",
+          website: "https://www.socialmoments.com",
+          img: "https://www.socialmoments/logo-img.png", createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "EdAble Flowers",
+          summary: "This organisation grows edible flowers for distribution",
+          description: null,
+          activeStatus: false,
+          ABN: "48588334938",
+          phone: "0493959487",
+          website: "https://www.edablelfowers.com",
+          img: "https://www.edablelfowers/logo-img.png", createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "Windy",
+          summary: "Windy is a non for profit organization that helps supply small wind turbines to unpriviled families ",
+          description: null,
+          activeStatus: true,
+          ABN: "49557732843",
+          phone: "0438827348",
+          website: "https://www.Windy.com",
+          img: "https://www.Windy/logo-img.png", createdAt: serverTimestamp(),
+          updatedAt: null
+        },
+        {
+
+          name: "Doin Doughies",
+          summary: "Doin Doughies bakes fresh donuts for the town of Narrandera daily",
+          description: null,
+          activeStatus: false,
+          ABN: "46392849511",
+          phone: "0489334598",
+          website: "https://www.doindoughies.com",
+          img: "https://www.doindoughies/logo-img.png", createdAt: serverTimestamp(),
+          updatedAt: null
+        }
+    ]
+
+    const organisationsCollection = this.firestore.collection('/Organisations')
+
+    orgsToAdd.forEach(async (org) => {
+      await organisationsCollection.add(org).catch(err => {console.log(err)})
+    })
+  }
+
+
+
+}
