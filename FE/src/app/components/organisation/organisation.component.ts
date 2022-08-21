f81b08bd6513515f64d9dd15937157d645938756
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-08-21 18:33:08 +1000

    Created GET Call for Organisations & Table Reload Upon Creating an Organisation

--- FE/src/app/components/organisation/organisation.component.ts
+++ FE/src/app/components/organisation/organisation.component.ts
@@ -1,6 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject, ViewChild } from '@angular/core';
-import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
@@ -20,11 +24,10 @@ export interface DialogData {
   description: string | undefined;
 }
 
-
 @Component({
   selector: 'organisation-add-dialog.component',
   templateUrl: 'organisation-add-dialog.component.html',
-  styleUrls: ['./organisation.component.scss']
+  styleUrls: ['./organisation.component.scss'],
 })
 export class AddOrganisationDialog {
   // passedvalues: object | undefined;
@@ -39,8 +42,8 @@ export class AddOrganisationDialog {
 
   constructor(
     public dialogRef: MatDialogRef<AddOrganisationDialog>,
-    @Inject(MAT_DIALOG_DATA) public data : DialogData,
-    private taskService: TaskService,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData,
+    private taskService: TaskService
   ) {}
 
   // createNewOrganisation(passedvalues) {
@@ -54,11 +57,10 @@ export class AddOrganisationDialog {
   }
 }
 
-
 @Component({
   selector: 'app-organisation',
   templateUrl: './organisation.component.html',
-  styleUrls: ['./organisation.component.scss']
+  styleUrls: ['./organisation.component.scss'],
 })
 export class OrganisationComponent {
   passedvalues: JSON | undefined;
@@ -69,55 +71,101 @@ export class OrganisationComponent {
   ABN: string | undefined;
   phone: string | undefined;
   website: string | undefined;
-  img: string = "INSERT Image URL";
+  img: string = 'INSERT Image URL';
   displayedColumns: string[] = ['name', 'activeItems', 'donations'];
   dataSource: any;
-  selectedRowIndex = "";
+  selectedRowIndex = '';
 
-
-  constructor(public dialog: MatDialog, private taskService: TaskService, public http: HttpClient) {
-  }
-
+  constructor(
+    public dialog: MatDialog,
+    private taskService: TaskService,
+    public http: HttpClient
+  ) {}
 
   addOrgDialog(): void {
     const dialogRef = this.dialog.open(AddOrganisationDialog, {
       width: '400px',
-      data: {name: this.name, summary: this.summary, activeStatus: this.activeStatus, ABN: this.ABN, phone: this.phone, website: this.website, img: this.img, description: this.description},
+      data: {
+        name: this.name,
+        summary: this.summary,
+        activeStatus: this.activeStatus,
+        ABN: this.ABN,
+        phone: this.phone,
+        website: this.website,
+        img: this.img,
+        description: this.description,
+      },
     });
 
-    dialogRef.afterClosed().subscribe(result => {
+    dialogRef.afterClosed().subscribe((result) => {
       console.log('The dialog was closed');
-      this.http.post('https://dip-challenge.azurewebsites.net/organisation', JSON.parse(JSON.stringify(result))).subscribe(
-        response => {
+      this.http
+        .post(
+          'https://dip-challenge.azurewebsites.net/organisation',
+          JSON.parse(JSON.stringify(result))
+        )
+        .subscribe((response) => {
           console.log(response);
-      });
+          this.getOrgs();
+        });
     });
   }
-
-
 
   orgs: Organisation[];
   items: Item[];
   activeItems: Item[];
+  orgData: any;
+  cleanOrgData: any;
 
   ngOnInit(): void {
     //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
     //Add 'implements OnInit' to the class.
+    // this.getOrgs().then( resp => console.log(resp));
     this.getOrgs();
+    // console.log(this.orgData);
+    // console.log(this.cleanOrgData);
+    // this.getOrganisations();
   }
 
+  getOrgs() {
+    this.http
+      .get<any>(
+        'https://dip-challenge.azurewebsites.net/organisation/dashboard'
+      )
+      .subscribe({
+        next: (resp) => {
+          (this.orgData = resp), console.log(resp);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+        complete: () => {
+          this.cleanOrgData = this.orgData.map((item: any) => {
+            let org = {
+              name: item._fieldsProto.name.stringValue,
+              activeItems: 0,
+              donations: 0,
+            };
 
-  getOrgs(){
-    this.http.get<any>('https://dip-challenge.azurewebsites.net/organisation/dashboard').subscribe(
-      response => {
-        console.log(response);
-        this.orgs = response;
-    });
-    }
+            return org;
+          });
+
+          console.log(this.cleanOrgData);
+        },
+      });
+  }
+
+  // getOrganisations(){
+  // this.http.get<any>('https://dip-challenge.azurewebsites.net/organisation/dashboard').subscribe(
+  //   response => {
+  //     console.log(response[0]._fieldsProto);
+  //     this.orgs = response;
+  //     console.log(this.orgs);
+  // });
+  // }
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
-
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
@@ -135,22 +183,23 @@ export class OrganisationComponent {
 
   selectRow(row: Organisation) {
     if (this.selectedRowIndex === row.id) {
-      this.selectedRowIndex = "";
+      this.selectedRowIndex = '';
       this.activeItems = [];
       return;
     }
     this.selectedRowIndex = row.id;
-    this.activeItems = this.items.filter(item => { return item.orgID === row.id });
+    this.activeItems = this.items.filter((item) => {
+      return item.orgID === row.id;
+    });
     console.log(this.activeItems);
-
   }
 }
 export interface Organisation {
   id: string;
   name: string;
-  activeItems: number;
+  activeItems?: number;
   inactiveItems: number;
-  donations: number;
+  donations?: number;
 }
 export interface Item {
   name: string;
@@ -158,8 +207,5 @@ export interface Item {
   totalDonations: number;
   activeStatus: boolean;
   orgID: string;
-}
-function createNewOrganisation(passedvalues: any) {
-  throw new Error('Function not implemented.');
 }
 
