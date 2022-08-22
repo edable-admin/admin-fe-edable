2dc2a27ad5189644cbe2416c4ae9e05951d887c1
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-08-22 19:02:27 +1000

    Linked Pagination, Search, Items to Organisations from API

--- FE/src/app/components/organisation/organisation.component.ts
+++ FE/src/app/components/organisation/organisation.component.ts
@@ -7,6 +7,7 @@ import {
 } from '@angular/material/dialog';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 
 export interface DialogData {
@@ -54,13 +55,26 @@ export class OrganisationComponent {
   website: string | undefined;
   img: string = 'INSERT Image URL';
   displayedColumns: string[] = ['name', 'activeItems', 'donations'];
-  dataSource: any;
   selectedRowIndex = '';
   orgs: Organisation[];
-  items: Item[];
   activeItems: Item[];
   orgData: any;
   cleanOrgData: any;
+  // dataSource: any;
+
+  items: Item[] = [
+    { name: "Shovel", initialPrice: 50, totalDonations: 10, activeStatus: true, orgID: 'EdAble Flowers', img: 'https://i.imgur.com/ioUzxDC.jpeg'},
+    { name: "Hose", initialPrice: 60, totalDonations: 5, activeStatus: true, orgID: 'EdAble Flowers', img: 'https://i.imgur.com/PFuUHCi.jpeg'},
+    { name: "Shovel", initialPrice: 50, totalDonations: 10, activeStatus: true, orgID: 'EdAble Flowers', img: 'https://i.imgur.com/ioUzxDC.jpeg'},
+    { name: "Hose", initialPrice: 60, totalDonations: 5, activeStatus: true, orgID: 'helloooooooo', img: 'https://i.imgur.com/PFuUHCi.jpeg'},
+    { name: "Shovel", initialPrice: 50, totalDonations: 10, activeStatus: true, orgID: '2', img: 'https://i.imgur.com/ioUzxDC.jpeg'},
+    { name: "Hose", initialPrice: 60, totalDonations: 5, activeStatus: true, orgID: '2', img: 'https://i.imgur.com/PFuUHCi.jpeg'},
+    { name: "Oven", initialPrice: 800, totalDonations: 100, activeStatus: true, orgID: 'Doin Doughies', img:  'https://i.imgur.com/IJ3ehgi.jpeg'},
+    { name: "Mixer", initialPrice: 300, totalDonations: 60, activeStatus: true, orgID: 'Windy', img:  'https://i.imgur.com/BTV0RRM.png'},
+    { name: "Polish", initialPrice: 40, totalDonations: 8, activeStatus: true, orgID: 'Windy', img: 'https://i.imgur.com/4TmqOIi.jpeg' },
+    { name: "Shoe laces", initialPrice: 50, totalDonations: 10, activeStatus: true, orgID: 'Windy', img:  'https://i.imgur.com/Cwtpkj4.jpeg'},
+  ]
+
 
   constructor(
     public dialog: MatDialog,
@@ -83,18 +97,20 @@ export class OrganisationComponent {
     });
 
     dialogRef.afterClosed().subscribe((result) => {
-      console.log('The dialog was closed');
+      // console.log('The dialog was closed');
       this.http
         .post(
           'https://dip-challenge.azurewebsites.net/organisation',
           JSON.parse(JSON.stringify(result))
         )
         .subscribe((response) => {
-          console.log(response);
           this.getOrgs();
         });
     });
   }
+
+  @ViewChild(MatPaginator,{static:true}) paginator: MatPaginator;;
+  @ViewChild(MatSort) sort: MatSort;
 
   ngOnInit(): void {
     this.getOrgs();
@@ -107,7 +123,7 @@ export class OrganisationComponent {
       )
       .subscribe({
         next: (resp) => {
-          (this.orgData = resp), console.log(resp);
+          (this.orgData = resp)
         },
         error: (err) => {
           console.log(err);
@@ -119,45 +135,43 @@ export class OrganisationComponent {
               activeItems: 0,
               donations: 0,
             };
-
             return org;
           });
-
-          console.log(this.cleanOrgData);
+          this.cleanOrgData = new MatTableDataSource(this.cleanOrgData);
+          this.cleanOrgData.paginator = this.paginator;
+          this.cleanOrgData.sort = this.sort;
         },
       });
   }
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  @ViewChild(MatSort) sort: MatSort;
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
-  }
+  // ngAfterViewInit() {
+  //   this.dataSource.paginator = this.paginator;
+  //   this.dataSource.sort = this.sort;
+  // }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
+    this.cleanOrgData.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
+    if (this.cleanOrgData.paginator) {
+      this.cleanOrgData.paginator.firstPage();
     }
   }
 
-  selectRow(row: Organisation) {
-    if (this.selectedRowIndex === row.id) {
+  selectRow(cleanOrgData) {
+    if (this.selectedRowIndex === cleanOrgData.name) {
       this.selectedRowIndex = '';
       this.activeItems = [];
       return;
     }
-    this.selectedRowIndex = row.id;
+    this.selectedRowIndex = cleanOrgData.name;
     this.activeItems = this.items.filter((item) => {
-      return item.orgID === row.id;
+      return item.orgID === cleanOrgData.name;
     });
-    console.log(this.activeItems);
+    // console.log(this.activeItems);
   }
 }
+
 export interface Organisation {
   id: string;
   name: string;
