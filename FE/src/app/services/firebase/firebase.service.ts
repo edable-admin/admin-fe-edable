ada961092ab18cf3dbc3c5625b8956e593c79d76
dan933 <dnadistrictservices@gmail.com>
2022-09-06 17:29:12 +1000

    removed unused imports

--- FE/src/app/services/firebase/firebase.service.ts
+++ FE/src/app/services/firebase/firebase.service.ts
@@ -2,10 +2,6 @@ import { Injectable, Type } from '@angular/core';
 import { AngularFireStorage } from '@angular/fire/compat/storage';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from '@angular/fire/compat/firestore';
 import { noSQLData } from './no-sql-data';
-import { doc, limit, query, QueryDocumentSnapshot } from 'firebase/firestore';
-import { getType } from '@angular/flex-layout/extended/style/style-transforms';
-import { EventType } from '@angular/router';
-import { throwError, timeout } from 'rxjs';
 import { Organisation } from 'src/app/models/Organisation/Organisation';
 
 @Injectable({
