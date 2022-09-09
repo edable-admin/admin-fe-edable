aa57cf11a7bbad37d515ba91507d8ee5abfd3505
dan933 <69749413+dan933@users.noreply.github.com>
2022-09-09 20:05:18 +1000

    added semicolons  to firebase services

--- FE/src/app/services/firebase/firebase.service.ts
+++ FE/src/app/services/firebase/firebase.service.ts
@@ -22,20 +22,20 @@ export class FirebaseService {
     //gets the organisation
     const org = this.fs
       .collection('Organisations')
-      .doc(orgID)
+      .doc(orgID);
 
     //gets the reference
-    const orgRef = org.ref
+    const orgRef = org.ref;
 
     const generalDonationsSummary = org
       .collection('GeneralDonations')
-      .doc('Summary')
+      .doc('Summary');
 
     //gets the general donation limited to 1
     const generalDonationsColl = generalDonationsSummary
       .collection('Donations', query =>
         query.limit(1)
-      )
+      );
 
     const generalDonationsRef = generalDonationsColl.ref
 
@@ -52,41 +52,40 @@ export class FirebaseService {
     //gets org data and assigns it to orgName
     await orgRef
       .get()
-      .then((org) => orgName = org.data()['name'])
+      .then((org) => orgName = org.data()['name']);
 
 
-    const generalDonationsQuery = generalDonationsRef.get()
-    const generalDonations:GeneralDonations[] = (await generalDonationsQuery).docs.map(x => x.data() as GeneralDonations);
+    const generalDonationsQuery = generalDonationsRef.get();
+    const generalDonations: GeneralDonations[] = (await generalDonationsQuery).docs.map(x => x.data() as GeneralDonations);
 
-    //todo response interface
     let response: Response;
 
     //if there is a general donation in an organisation
     if (generalDonations.length > 0) {
-      response = { message: `${orgName} cannot be deleted as it has general donations`}
+      response = { message: `${orgName} cannot be deleted as it has general donations` };
       return response;
     }
 
 
     const itemsQuery = itemsRef.get();
-    const items = (await itemsQuery).docs.map(x => x.data() as Item)
+    const items = (await itemsQuery).docs.map(x => x.data() as Item);
 
     // if there is an item in the organisation return message
     if (items.length > 0) {
-      response = { message: `${orgName} cannot be deleted as it has donation items`}
+      response = { message: `${orgName} cannot be deleted as it has donation items` };
       return response;
-    }
+    };
 
     //If organisation does not have items or donations delete organisations
     generalDonationsSummary.delete();
-    org.delete()
+    org.delete();
 
     this.storage.ref(`Organisations/${orgID}/orgLogo`).delete().subscribe({
       error: (err) => console.log(err)
-    })
+    });
 
 
-    response = {message: `${orgName} Successfully Deleted`}
+    response = { message: `${orgName} Successfully Deleted` };
 
     return response;
   }
@@ -94,64 +93,63 @@ export class FirebaseService {
   getOrgs(activeStatus:boolean) {
     let orgs = this.fs
       .collection('Organisations', query =>
-      query.where('activeStatus',"==", activeStatus))
-      .valueChanges({idField:"id"})
-    return orgs
+        query.where('activeStatus', "==", activeStatus))
+      .valueChanges({ idField: "id" });
+    return orgs;
 
   }
 
   async addOrganisation(orgData: Organisation) {
-    console.log(orgData)
 
-    const orgReq:Organisation = {
+    const orgReq: Organisation = {
       name: orgData.name ? orgData.name : null,
-      summary: orgData.summary ? orgData.summary: null,
-      description: orgData.description ? orgData.description: null,
-      activeStatus: orgData.activeStatus ? orgData.activeStatus: true,
-      ABN: orgData.ABN ? orgData.ABN: null,
-      phone: orgData.phone ? orgData.phone: null,
+      summary: orgData.summary ? orgData.summary : null,
+      description: orgData.description ? orgData.description : null,
+      activeStatus: orgData.activeStatus ? orgData.activeStatus : true,
+      ABN: orgData.ABN ? orgData.ABN : null,
+      phone: orgData.phone ? orgData.phone : null,
       website: orgData.website ? orgData.website : null,
       img: orgData.img ? orgData.img : null,
       totalDonationItems: 0,
-      totalDonations:0
-    }
+      totalDonations: 0
+    };
 
     let generalDonationReq = {
       totalGeneralDonations: 0,
-      numberOfDonations:0
-    }
+      numberOfDonations: 0
+    };
 
     let orgRef = this.fs
       .collection('Organisations')
-      .doc().ref
+      .doc().ref;
 
     if (orgData?.file) {
       this.uploadImage(orgRef.id, orgData.file)
-    }
+    };
 
     let generalDonationsRef = orgRef
       .collection('GeneralDonations')
-      .doc('Summary')
+      .doc('Summary');
 
-    let batch = this.fs.firestore.batch()
+    let batch = this.fs.firestore.batch();
 
     //adds the org
-    batch.set(orgRef, orgReq,{merge:true})
+    batch.set(orgRef, orgReq, { merge: true });
 
     //adds the general donations subcollection
-    batch.set(generalDonationsRef, generalDonationReq)
+    batch.set(generalDonationsRef, generalDonationReq);
 
     //commits the batch
-    batch.commit()
+    batch.commit();
 
     //returns success message
-    return {orgRef:orgRef.id,message:`${orgReq.name} Successfully Added`}
+    return { orgRef: orgRef.id, message: `${orgReq.name} Successfully Added` };
   }
 
   async editOrganisation(orgID: string, organisationReq: Organisation) {
 
     this.fs.collection('Organisations').doc(orgID)
-      .update(organisationReq)
+      .update(organisationReq);
 
     return organisationReq;
   }
@@ -161,21 +159,19 @@ export class FirebaseService {
       return false;
     }
 
-    const regImage = /image\/.*/g
+    const regImage = /image\/.*/g;
 
     if (img.length === 1 && regImage.test(img[0].type)) {
       return true;
     } else {
       return false;
-    }
+    };
 
-  }
+  };
 
   async uploadImage(orgRef: string, file: FileList, itemRef?: string) {
 
-
-    let imgLocation = itemRef ? `Organisations/${orgRef}/Items/${itemRef}/itemImg`
-      : `Organisations/${orgRef}/orgLogo`;
+    let imgLocation = itemRef ? `Organisations/${orgRef}/Items/${itemRef}/itemImg`:`Organisations/${orgRef}/orgLogo`;
 
     let imgURL:string;
 
@@ -194,15 +190,14 @@ export class FirebaseService {
         .getDownloadURL()
         .then(async (url) => {
           if (itemRef) {
-            await item.set({img: url}, {merge:true})
+            await item.set({ img: url }, { merge: true });
           }else{
-            await org.set({ img: url }, { merge: true })
+            await org.set({ img: url }, { merge: true });
           }
-          imgURL = url
+          imgURL = url;
         }).catch(err => console.log(err));
     }
 
     return imgURL;
   }
-
 }
