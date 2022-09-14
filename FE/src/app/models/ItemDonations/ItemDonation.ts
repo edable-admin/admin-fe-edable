aa00372f7cced1c4dd2eda76ffb0eb14b1ff6764
Waderaide <103882777@student.swin.edu.au>
2022-09-14 15:26:48 +1000

    added models

--- FE/src/app/models/ItemDonations/ItemDonation.ts
+++ FE/src/app/models/ItemDonations/ItemDonation.ts
@@ -1,7 +1,6 @@
 import { Timestamp } from "firebase/firestore";
 
 export interface GeneralDonations {
-  IsSubscribed: boolean,
   IsRefunded: boolean,
   donationDate: Timestamp,
   paidAMT: number,
--- FE/src/app/services/firebase/transaction-service/transaction.service.ts
+++ FE/src/app/services/firebase/transaction-service/transaction.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
 
 @Injectable({
   providedIn: 'root'
