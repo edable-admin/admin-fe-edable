fc514baad0a737bf73b0be282df7b6e6a3e40db9
Waderaide <103882777@student.swin.edu.au>
2022-09-14 11:16:31 +1000

    added search box and table w/temp data

--- FE/src/app/components/transactions/transactions.component.ts
+++ FE/src/app/components/transactions/transactions.component.ts
@@ -1,11 +1,34 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  date: string;
+  name: string;
+  email: string;
+  donationItem: string;
+  donationAmount: number;
+  organisation: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  {date: "08/06/2022", name: "Jeff", email: "test@test.com", donationItem: "Flour", donationAmount: 200, organisation: "Barry's Bakehouse"},
+  
+  
+];
+
 @Component({
   selector: 'app-transactions',
   templateUrl: './transactions.component.html',
   styleUrls: ['./transactions.component.scss']
 })
 export class TransactionsComponent implements OnInit {
+
+  dataSource = ELEMENT_DATA;
+  displayedColumns: string[] = ['date', 'name', 'email', 'donationItem', 'donationAmount', 'organisation'];
 
   constructor() { }
 
