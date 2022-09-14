58536e2bbfa3a2a039ca785b12c6b25720d21bdd
dan933 <dnadistrictservices@gmail.com>
2022-09-14 18:55:18 +1000

    added new component for view-donation-item

---
+++ FE/src/app/components/donation-item/view-donation-item/view-donation-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-donation-item',
+  templateUrl: './view-donation-item.component.html',
+  styleUrls: ['./view-donation-item.component.scss']
+})
+export class ViewDonationItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
