578fd90f1677389d06ab4b724eb02c9fb134a0ec
dan933 <69749413+dan933@users.noreply.github.com>
2022-09-15 14:53:09 +1000

    removed duplicates

--- FE/src/app/components/transactions/transactions.component.ts
+++ FE/src/app/components/transactions/transactions.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { TransactionService } from 'src/app/services/firebase/transaction-service/transaction.service';
 import { Subscription } from 'rxjs';
 import { ItemDonations } from 'src/app/models/ItemDonations/ItemDonation';
-import { TransactionService } from 'src/app/services/firebase/transaction-service/transaction.service';
-import { Subscription } from 'rxjs';
-import { ItemDonations } from 'src/app/models/ItemDonations/ItemDonation';
 
 @Component({
   selector: 'app-transactions',
@@ -34,32 +31,5 @@ export class TransactionsComponent implements OnInit {
   getItemDonations() {
     this.ts.getItemDonations().then((resp) => {resp.docs.forEach(resp => resp.data())});
 
-  }
-
-  //-------------------- Get item donations for singular org --------------------\\
-  getOrgItemDonations(orgID:string, ItemID:string) {
-        this.ts.getOrgItemDonations(orgID, ItemID).then((resp) => {resp.docs.forEach(resp => resp.data())});
-  }
-
-  //-------------------- Get General Donations for org --------------------------\\
-  getOrgGenDonations(orgID:string) {
-    this.ts.getOrgGeneralDonations(orgID).then((resp) => {resp.docs.forEach(resp => resp.data())});
-  }
-  //-------------------- Get All General Donations for --------------------------\\
-  getGenDonations() {
-    this.ts.getGeneralDonations().then((resp) => {resp.docs.forEach(resp => resp.data())});
-  }
-  //-------------------- Get all item donations ---------------------------------\\
-  getItemDonations() {
-    this.ts.getItemDonations().then((resp) => {resp.docs.forEach(resp => resp.data())});
-  }
-
-  //-------------------- Get General Donations for org --------------------------\\
-  getOrgGenDonations(orgID:string) {
-    this.ts.getOrgGeneralDonations(orgID).then((resp) => {resp.docs.forEach(resp => resp.data())});
-  }
-  //-------------------- Get All General Donations for --------------------------\\
-  getGenDonations() {
-    this.ts.getGeneralDonations().then((resp) => {resp.docs.forEach(resp => resp.data())});
   }
 }
