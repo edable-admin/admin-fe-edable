4166deabef1754954e10da38955ddc6bb9aa61d6
Stav-Kyriacou <101610510@student.swin.edu.au>
2022-10-25 14:41:47 +1100

    add key to staging environment

--- FE/src/environments/environment.staging.ts
+++ FE/src/environments/environment.staging.ts
@@ -20,5 +20,6 @@ export const environment = {
     messagingSenderId: "217658206897",
     appId: "1:217658206897:web:89b86a67919a0d697ad090",
     measurementId: "G-5D01P8RC1Z"
-  }
+  },
+  recaptcha3SiteKey:"6LetWpUiAAAAALG0r3OrdNPXmVFV61zXEMHvI5Fh"
 };
